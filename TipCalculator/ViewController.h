6ae4501c20c79a9620718689fc571e162e3e38d4
Renato Camilio <renatomcamilio@gmail.com>
2015-01-23 12:51:00 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Renato Camilio on 1/23/15.
+//  Copyright (c) 2015 Renato Camilio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
